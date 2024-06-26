86469aad0052ccff568bf57d60e7ed8d0387f37e
Lark <63553103+user-lark@users.noreply.github.com>
2024-06-26 10:43:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const http = require("http");
-const mineflayer = require('mineflayer');
-const pvp = require('mineflayer-pvp').plugin;
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
-const armorManager = require('mineflayer-armor-manager');
+const mineflayer = require('mineflayer')
+const pvp = require('mineflayer-pvp').plugin
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const armorManager = require('mineflayer-armor-manager')
 const AutoAuth = require('mineflayer-auto-auth');
 const app = express();
 
@@ -16,223 +16,112 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.repl.co/`);
 }, 224000);
 
+// U CAN ONLY EDIT THIS SECTION!!
 function createBot() {
   const bot = mineflayer.createBot({
     host: 'tirto.aternos.me',
-    version: false,
+    version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
     username: 'Tirtobot',
     port: 23621,
     plugins: [AutoAuth],
     AutoAuth: 'bot112022'
-  });
-
-  bot.loadPlugin(pvp);
-  bot.loadPlugin(armorManager);
-  bot.loadPlugin(pathfinder);
-
-  // Kirim ping ke server secara berkala
-  setInterval(() => {
-    if (bot && bot.connected) {
-      bot.tabComplete('', (err, tabComplete) => {
-        if (err) console.error('Error pinging server:', err);
-      });
-    }
-  }, 60000); // Setiap 60 detik (1 menit)
+  })
+  /// DONT TOUCH ANYTHING MORE!
+  bot.loadPlugin(pvp)
+  bot.loadPlugin(armorManager)
+  bot.loadPlugin(pathfinder)
 
   bot.on('playerCollect', (collector, itemDrop) => {
-    if (collector !== bot.entity) return;
+    if (collector !== bot.entity) return
 
     setTimeout(() => {
-      const sword = bot.inventory.items().find(item => item.name.includes('sword'));
-      if (sword) bot.equip(sword, 'hand');
-    }, 150);
-  });
+      const sword = bot.inventory.items().find(item => item.name.includes('sword'))
+      if (sword) bot.equip(sword, 'hand')
+    }, 150)
+  })
 
   bot.on('playerCollect', (collector, itemDrop) => {
-    if (collector !== bot.entity) return;
+    if (collector !== bot.entity) return
 
     setTimeout(() => {
-      const shield = bot.inventory.items().find(item => item.name.includes('shield'));
-      if (shield) bot.equip(shield, 'off-hand');
-    }, 250);
-  });
+      const shield = bot.inventory.items().find(item => item.name.includes('shield'))
+      if (shield) bot.equip(shield, 'off-hand')
+    }, 250)
+  })
 
-  let guardPos = null;
+  let guardPos = null
 
   function guardArea(pos) {
-    guardPos = pos.clone();
+    guardPos = pos.clone()
 
     if (!bot.pvp.target) {
-      moveToGuardPos();
+      moveToGuardPos()
     }
   }
 
   function stopGuarding() {
-    guardPos = null;
-    bot.pvp.stop();
-    bot.pathfinder.setGoal(null);
+    guardPos = null
+    bot.pvp.stop()
+    bot.pathfinder.setGoal(null)
   }
 
   function moveToGuardPos() {
-    const mcData = require('minecraft-data')(bot.version);
-    bot.pathfinder.setMovements(new Movements(bot, mcData));
-    bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z));
+    const mcData = require('minecraft-data')(bot.version)
+    bot.pathfinder.setMovements(new Movements(bot, mcData))
+    bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z))
   }
 
   bot.on('stoppedAttacking', () => {
     if (guardPos) {
-      moveToGuardPos();
+      moveToGuardPos()
     }
-  });
+  })
 
   bot.on('physicTick', () => {
-    if (bot.pvp.target) return;
-    if (bot.pathfinder.isMoving()) return;
+    if (bot.pvp.target) return
+    if (bot.pathfinder.isMoving()) return
 
-    const entity = bot.nearestEntity();
-    if (entity) bot.lookAt(entity.position.offset(0, entity.height, 0));
-  });
+    const entity = bot.nearestEntity()
+    if (entity) bot.lookAt(entity.position.offset(0, entity.height, 0))
+  })
 
   bot.on('physicTick', () => {
-    if (!guardPos) return;
+    if (!guardPos) return
     const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 16 &&
-      e.mobType !== 'Armor Stand';
-    const entity = bot.nearestEntity(filter);
+      e.mobType !== 'Armor Stand'
+    const entity = bot.nearestEntity(filter)
     if (entity) {
-      bot.pvp.attack(entity);
+      bot.pvp.attack(entity)
     }
-  });
-
-  let isFishing = false;
-  let isStationary = false;
+  })
 
   bot.on('chat', (username, message) => {
     if (message === 'guard') {
-      const player = bot.players[username];
+      const player = bot.players[username]
 
-      if (player) {
-        bot.chat('I will!');
-        guardArea(player.entity.position);
+      if (!player) {
+        bot.chat('I will!')
+        guardArea(player.entity.position)
       }
+
     }
     if (message === 'stop') {
-      bot.chat('I will stop!');
-      stopGuarding();
+      bot.chat('I will stop!')
+      stopGuarding()
     }
-    if (message === 'mancing') {
-      bot.chat('Fishing time!');
-      isFishing = true;
-      startFishing();
-    }
-    if (message === 'stop mancing') {
-      bot.chat('I will stop fishing!');
-      isFishing = false;
-    }
-    if (message === 'bagi hasil') {
-      const player = bot.players[username];
-      if (player) {
-        bot.chat('I will share my catch!');
-        shareFishingLoot(player);
-      } else {
-        bot.chat('I cannot see you!');
-      }
-    }
-    if (message === 'diam') {
-      bot.chat('I will stay here!');
-      isStationary = true;
-      stayStill();
-    }
-    if (message === 'gerak') {
-      bot.chat('I will start moving again!');
-      isStationary = false;
-    }
-  });
+  })
 
-  bot.on('kicked', (reason, loggedIn) => {
-    if (loggedIn) {
-      console.log('Kicked from server. Reconnecting...');
-      setTimeout(createBot, 5000); // Reconnect after 5 seconds
-    }
-  });
-
-  bot.on('error', (err) => {
-    console.error('Bot error:', err);
-  });
+  bot.on('kicked', console.log)
+  bot.on('error', console.log)
 
   bot.on('end', () => {
-    console.log('Disconnected from server. Reconnecting...');
-    setTimeout(createBot, 5000); // Reconnect after 5 seconds
+    console.log("Bot disconnected. Attempting to reconnect in 10 seconds...");
+    setTimeout(createBot, 10000); // Retry connecting after 10 seconds
   });
-
-  bot.on('death', () => {
-    console.log('Bot was killed. Reconnecting...');
-    setTimeout(createBot, 5000); // Reconnect after 5 seconds
-  });
-
-  const actions = {
-    commands: ['forward', 'back', 'left', 'right'],
-    holdDuration: 1000 // Durasi tahan perintah dalam milidetik
-  };
-
-  function performAction() {
-    if (isStationary) return;
-    const command = actions.commands[Math.floor(Math.random() * actions.commands.length)];
-    bot.setControlState(command, true);
-    setTimeout(() => {
-      bot.setControlState(command, false);
-    }, actions.holdDuration);
-  }
-
-  bot.on('physicTick', () => {
-    if (Math.random() < 0.01) { // roughly every 100 ticks
-      performAction();
-    }
-  });
-
-  function startFishing() {
-    if (!isFishing) return;
-    const fishingRod = bot.inventory.items().find(item => item.name.includes('fishing_rod'));
-    if (!fishingRod) {
-      bot.chat('I do not have a fishing rod!');
-      isFishing = false;
-      return;
-    }
-    bot.equip(fishingRod, 'hand', (err) => {
-      if (err) {
-        bot.chat('Error equipping fishing rod');
-        isFishing = false;
-        return;
-      }
-      bot.activateItem();
-      setTimeout(() => {
-        bot.deactivateItem();
-        if (isFishing) {
-          setTimeout(startFishing, 3000); // Fish again after 3 seconds
-        }
-      }, 30000); // Fish for 30 seconds
-    });
-  }
-
-  function shareFishingLoot(player) {
-    const fishTypes = ['cod', 'salmon', 'pufferfish', 'tropical_fish'];
-    const fish = bot.inventory.items().find(item => fishTypes.includes(item.name));
-
-    if (fish) {
-      bot.tossStack(fish, (err) => {
-        if (err) {
-          bot.chat('Failed to toss the item');
-        } else {
-          bot.chat('Here is some fish for you!');
-        }
-      });
-    } else {
-      bot.chat('I have no fish to share!');
-    }
-  }
-
-  function stayStill() {
-    bot.clearControlStates();
-  }
 }
 
-createBot();
+createBot()
+
+//// Remember to subscribe to my channels!
+/// www.youtube.com/c/JinMoriYT
+/// www.youtube.com/channel/UC1SR0lQSDfdaSMhmUiaMitg
